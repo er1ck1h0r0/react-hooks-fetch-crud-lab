aa5004d11e9bd0269d7cdd7190008f9ae15582ce
eric <ericmaina11455@gmail.com>
2024-05-03 12:57:42 +0300

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions,setQuestions]= useState([])
+
+  
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/questions")
+    .then(r => r.json())
+    .then(data =>{
+      console.log(data)
+      return setQuestions(data)
+      
+    })
+  
+  
+  },[])
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm questions={questions} setQuestions={setQuestions}/> : <QuestionList questions={questions} setQuestions={setQuestions}/>}
     </main>
   );
 }
